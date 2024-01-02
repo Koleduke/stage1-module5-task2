90a480a1f9487d81d61e38bdc1d5f7c9df69024c
Koleduke <Ctvm1234>
2024-01-02 20:40:45 +0300

    done

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,20 @@
 package com.epam.mjc;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return new Operation<Integer>() {
+            @Override
+            public List<Integer> apply(List<Integer> arg) {
+                List<Integer> list = new LinkedList<>();
+                for (int i =0; arg.size()>i;i++){
+                    list.add(arg.get(i)/divider);
+                }
+                return list;
+            }
+        };
     }
 }
